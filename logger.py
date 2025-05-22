f1d3d5f4fb7f47614b551af11d02d8490a7f1687
MoraGomora <moragomora30@gmail.com>
2025-05-22 17:35:16 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,19 +3,19 @@ import datetime
 import os
 
 class Log():
-    def __init__(self, name: str | None = None, level = logging.DEBUG):
+    def __init__(self, name: str | None = "userbot-moderator", level = logging.DEBUG):
         self._log_dir = "logs"
         self._log_name = f"app_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+        self._level = level
 
-        self._logger = logging.getLogger(name if name else "userbot-moderator")
-        self._logger.setLevel(level)
-        self._formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        self._logger = logging.getLogger(name)
 
-        self.console_handler = logging.StreamHandler()
-        self.console_handler.setFormatter(self._formatter)
-        self.console_handler.setLevel(logging.INFO)
-
-        self._logger.addHandler(self.console_handler)
+        if not self._logger.handlers:
+            self.console_handler = logging.StreamHandler()
+            self._formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+            self.console_handler.setFormatter(self._formatter)
+            self.console_handler.setLevel(self._level)
+            self._logger.addHandler(self.console_handler)
 
     def write_logs_to_file(self):
         if not os.path.exists(self._log_dir):
@@ -23,7 +23,7 @@ class Log():
 
         file_handler = logging.FileHandler(f"{self._log_dir}/{self._log_name}", encoding="utf-8")
         file_handler.setFormatter(self._formatter)
-        file_handler.setLevel(logging.DEBUG)
+        file_handler.setLevel(self._level)
 
         self._logger.addHandler(file_handler)
 
