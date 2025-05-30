92336ca04c89705561808f41cf168f522e9b38a5
MoraGomora <moragomora30@gmail.com>
2025-05-30 11:41:59 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,3 +9,13 @@ def get_owner_id() -> int:
     if load_dotenv():
         data = dotenv_values()
         return data["OWNER_ID"]
+    
+def get_ionet_key() -> str:
+    if load_dotenv():
+        data = dotenv_values()
+        return data["API_IONET_KEY"]
+    
+def get_mongodb_uri() -> str:
+    if load_dotenv():
+        data = dotenv_values()
+        return data["MONGODB_URI"]
