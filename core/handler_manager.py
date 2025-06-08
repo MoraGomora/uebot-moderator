6e32a2067b2724bf14f56d3ca13a1b4656430593
MoraGomora <moragomora30@gmail.com>
2025-06-08 11:27:33 +0300

    Update handler_manager.py

--- core/handler_manager.py
+++ core/handler_manager.py
@@ -11,28 +11,36 @@ async def register_handlers(client):
 
     from handlers.public import group_commands
 
-    from handlers.admin.group import access
-    from handlers.admin.group import blocked_users
+    from handlers.admin.group import access, blocked_users, trusted_users
+    from handlers.public.group import message, user
 
     from .plugin._initializer import _PluginCommandInializer
 
-    # ---------------- Initialize the command register ----------------
+    # ---- Initialize command register and group commands ----
     command_register = _PluginCommandInializer()
+
+    commands_group = group_commands.GroupCommands(client)
+    trusted_user = trusted_users.TrustedUsers(client)
+
     # ----------------- Register all handlers -----------------
     command_register._register_handlers(client)
 
-    # ---------------- REGISTER USER COMMANDS ----------------
+    # ----------------- REGISTER USER COMMANDS -----------------
     client.add_handler(MessageHandler(test.test, filters.command("test", prefixes=".") & filters.me & check_access_control(CommandAccessLevel.PRIVATE)))
     client.add_handler(MessageHandler(type.type, filters.command("type", prefixes=".") & filters.me & check_access_control(CommandAccessLevel.USER)))
     client.add_handler(MessageHandler(flip.flip, filters.command("flip", prefixes=".") & filters.me & check_access_control(CommandAccessLevel.USER)))
 
-    # ----------------- REGISTER GROUP COMMANDS -----------------
-    client.add_handler(MessageHandler(group_commands.message_data, filters.command("messageinfo", prefixes=".") & is_chat_allowed & check_access_control(CommandAccessLevel.PUBLIC)))
-    client.add_handler(MessageHandler(group_commands.user_info, filters.command("userinfo", prefixes=".") & is_chat_allowed & check_access_control(CommandAccessLevel.PUBLIC)))
+    # ------------- REGISTER GROUP PUBLIC COMMANDS -------------
+    client.add_handler(MessageHandler(message.message_data, filters.command("messageinfo", prefixes=".") & is_chat_allowed & check_access_control(CommandAccessLevel.PUBLIC)))
+    client.add_handler(MessageHandler(user.user_info, filters.command("userinfo", prefixes=".") & is_chat_allowed & check_access_control(CommandAccessLevel.PUBLIC)))
 
-    # ----------------- REGISTER ADMIN COMMANDS -----------------
-    client.add_handler(MessageHandler(access.allow_chat, filters.command("allow", prefixes=".") & check_access_control(CommandAccessLevel.PRIVATE) & is_admin))
-    client.add_handler(MessageHandler(access.disallow_chat, filters.command("disallow", prefixes=".") & check_access_control(CommandAccessLevel.PRIVATE) & is_admin))
-    client.add_handler(MessageHandler(blocked_users.get_blocked_users, filters.command("get_blocked", prefixes=".") & check_access_control(CommandAccessLevel.PRIVATE) & is_admin))
-    # client.add_handler(MessageHandler(group_commands.restrict_process, check_access_control(CommandAccessLevel.PUBLIC) & is_admin))
-    # client.add_handler(MessageHandler(group_commands.unrestrict_process, filters.command("unrestrict", prefixes=".") & check_access_control(CommandAccessLevel.PUBLIC) & is_admin))
+    # ------------- REGISTER ADMIN PUBLIC COMMANDS -------------
+    client.add_handler(MessageHandler(access.allow_chat, filters.command("allow", prefixes=".") & check_access_control(CommandAccessLevel.PUBLIC) & is_admin))
+    client.add_handler(MessageHandler(access.disallow_chat, filters.command("disallow", prefixes=".") & check_access_control(CommandAccessLevel.PUBLIC) & is_admin))
+    client.add_handler(MessageHandler(blocked_users.get_blocked_users, filters.command("get_blocked", prefixes=".") & check_access_control(CommandAccessLevel.PUBLIC) & is_chat_allowed & is_admin))
+
+    client.add_handler(MessageHandler(trusted_user.add_trusted_user, filters.command("add_trusted", prefixes=".") & check_access_control(CommandAccessLevel.PUBLIC) & is_chat_allowed & is_admin))
+    client.add_handler(MessageHandler(trusted_user.remove_trusted_user, filters.command("remove_trusted", prefixes=".") & check_access_control(CommandAccessLevel.PUBLIC) & is_chat_allowed & is_admin))
+    # client.add_handler(MessageHandler(trusted_users.list_trusted_users, filters.command("list_trusted", prefixes=".") & check_access_control(CommandAccessLevel.PRIVATE) & is_admin & is_chat_allowed))
+
+    client.add_handler(MessageHandler(commands_group.restrict_process, is_chat_allowed & is_admin))
